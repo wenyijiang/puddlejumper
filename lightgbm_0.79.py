c2d53d209e65c212f0c992547c98a3f1337b78bc
yuzhiweilai <qian.wang@smail.nju.edu.cn>
2018-07-24 19:12:39 +0800

    add lightgbm improvement code

--- lightgbm_0.79.py
+++ lightgbm_0.79.py
@@ -345,7 +345,7 @@ def kfold_lightgbm(df, num_folds, stratified=False):
     print('Full AUC score %.6f' % roc_auc_score(train_df['TARGET'], oof_preds))
     # Write submission file and plot feature importance
     test_df['TARGET'] = sub_preds
-    test_df[['SK_ID_CURR', 'TARGET']].to_csv("C:\\Users\\wq\\Desktop\\kaggle_credict_data\\data\\lightgbm_improvement.csv", index=False)
+    test_df[['SK_ID_CURR', 'TARGET']].to_csv(submission_file_name, index=False)
     display_importances(feature_importance_df)
     return feature_importance_df
 
@@ -360,43 +360,45 @@ def display_importances(feature_importance_df_):
     plt.tight_layout()
     plt.savefig('lgbm_importances-01.png')
 
-    def main(debug=False):
-        num_rows = 10000 if debug else None
-        df = application_train_test(num_rows)
-        with timer("Process bureau and bureau_balance"):
-            bureau = bureau_and_balance(num_rows)
-            print("Bureau df shape:", bureau.shape)
-            df = df.join(bureau, how='left', on='SK_ID_CURR')
-            del bureau
-            gc.collect()
-        with timer("Process previous_applications"):
-            prev = previous_applications(num_rows)
-            print("Previous applications df shape:", prev.shape)
-            df = df.join(prev, how='left', on='SK_ID_CURR')
-            del prev
-            gc.collect()
-        with timer("Process POS-CASH balance"):
-            pos = pos_cash(num_rows)
-            print("Pos-cash balance df shape:", pos.shape)
-            df = df.join(pos, how='left', on='SK_ID_CURR')
-            del pos
-            gc.collect()
-        with timer("Process installments payments"):
-            ins = installments_payments(num_rows)
-            print("Installments payments df shape:", ins.shape)
-            df = df.join(ins, how='left', on='SK_ID_CURR')
-            del ins
-            gc.collect()
-        with timer("Process credit card balance"):
-            cc = credit_card_balance(num_rows)
-            print("Credit card balance df shape:", cc.shape)
-            df = df.join(cc, how='left', on='SK_ID_CURR')
-            del cc
-            gc.collect()
-        with timer("Run LightGBM with kfold"):
-            feat_importance = kfold_lightgbm(df, num_folds=5, stratified=False)
 
-    if __name__ == "__main__":
-        submission_file_name = "submission_kernel26.csv"
-        with timer("Full model run"):
-            main(debug=False)
+
+def main(debug = False):
+    num_rows = 10000 if debug else None
+    df = application_train_test(num_rows)
+    with timer("Process bureau and bureau_balance"):
+        bureau = bureau_and_balance(num_rows)
+        print("Bureau df shape:", bureau.shape)
+        df = df.join(bureau, how='left', on='SK_ID_CURR')
+        del bureau
+        gc.collect()
+    with timer("Process previous_applications"):
+        prev = previous_applications(num_rows)
+        print("Previous applications df shape:", prev.shape)
+        df = df.join(prev, how='left', on='SK_ID_CURR')
+        del prev
+        gc.collect()
+    with timer("Process POS-CASH balance"):
+        pos = pos_cash(num_rows)
+        print("Pos-cash balance df shape:", pos.shape)
+        df = df.join(pos, how='left', on='SK_ID_CURR')
+        del pos
+        gc.collect()
+    with timer("Process installments payments"):
+        ins = installments_payments(num_rows)
+        print("Installments payments df shape:", ins.shape)
+        df = df.join(ins, how='left', on='SK_ID_CURR')
+        del ins
+        gc.collect()
+    with timer("Process credit card balance"):
+        cc = credit_card_balance(num_rows)
+        print("Credit card balance df shape:", cc.shape)
+        df = df.join(cc, how='left', on='SK_ID_CURR')
+        del cc
+        gc.collect()
+    with timer("Run LightGBM with kfold"):
+        feat_importance = kfold_lightgbm(df, num_folds= 5, stratified = False)
+
+if __name__ == "__main__":
+    submission_file_name = "C:\\Users\\wq\\Desktop\\kaggle_credict_data\\data\\lightgbm_improvement.csv"
+    with timer("Full model run"):
+        main(debug= False)
